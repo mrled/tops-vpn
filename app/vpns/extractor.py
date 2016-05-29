a3532e11204f93f840d87a732bcdbd40358054a2
Micah R Ledbetter <mrled@younix.us>
2016-05-29 18:53:19 -0500

    extractor now handles TOPS VPN categories

--- app/vpns/extractor.py
+++ app/vpns/extractor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-# Take a CSV export and turn it into a set of JSON files
-# (Which are easier to use as an Angular model)
+"""
+Take a CSV export and turn it into a set of JSON files
+(Which are easier to use as an Angular model)
+"""
 
 # TODO: Make file encoding errors return a user readable error
 # NOTE: If you have problems reading the CSV file, make sure it is saved as
@@ -14,17 +16,70 @@ import os
 import sys
 
 
+tops_categories = [
+  # "VPN SERVICE", # Leave this out!
+  "ACTIVISM",
+  "AFFILIATES",
+  "AVAILABILITY",
+  "ETHICS",
+  "JURISDICTION",
+  "LEAK PROTECTION",
+  "LOGGING",
+  "POLICIES",
+  "PORT BLOCKING",
+  "PRICING",
+  "PROTOCOLS",
+  "SECURITY",
+  "WEBSITE"
+]
+
+
+def parse_tops_vpn_csv_row(dictionary):
+    """
+    Parse a TOPS VPN CSV row
+    The TOPS VPN CSV represents a table where the columns are features and the
+    rows are VPN services. The features columns are organized into categories,
+    but the table is flat, so the features columns include entries like
+    'SECURITY Strongest Data Encryption' and 'SECURITY Weakest Data Encryption'.
+    Here we parse these categories and return a dictionary representing all
+    the categories, where each category contains individual features. Note that
+    if we find a feature that doesn't seem to match a category, it is added
+    directly.
+    """
+    outdict = {}
+    for key, value in dictionary.items():
+        found_category = False
+        for category in tops_categories:
+            if key.startswith(category):
+                found_category = True
+                if category not in outdict.keys():
+                    outdict[category] = {}
+                skipchars = len(category) +1
+                subkey = key[skipchars:]
+                outdict[category][subkey] = value
+        if not found_category:
+            outdict[key] = value
+    return outdict
+
+
 def extract_tops_vpn(filename):
+    """
+    Extract TOPS VPN entries from a CSV file
+    """
     providers = []
     csv.register_dialect('TopsVpn', csv.excel, quoting=csv.QUOTE_ALL)
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile, dialect="TopsVpn")
         for row in reader:
-            providers += [row]
+            providers += [parse_tops_vpn_dictionary(row)]
     return providers
 
 
 def save_vpns_as_json(vpnlist, outdir):
+    """
+    Save a list of dictionaries to a set of files on the filesystem
+    Use the VPN SERVICE key of the dictionary as the filename
+    """
     for vpn in vpnlist:
         filename = "{}.json".format(vpn['VPN SERVICE']).replace(' ', '').replace('/','-')
         outpath = os.path.abspath(os.path.join(outdir, filename))
