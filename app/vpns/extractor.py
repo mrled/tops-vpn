e526af968d6bb893bffc20c1023eb9a5dca9cc82
Micah R Ledbetter <mrled@younix.us>
2016-05-29 20:05:41 -0500

    extractor: save summary & generate ID

--- app/vpns/extractor.py
+++ app/vpns/extractor.py
@@ -17,7 +17,7 @@ import sys
 
 
 tops_categories = [
-  # "VPN SERVICE", # Leave this out!
+  # "VPN SERVICE",    # Leave this out! It's not a category, it's a whole field
   "ACTIVISM",
   "AFFILIATES",
   "AVAILABILITY",
@@ -34,6 +34,15 @@ tops_categories = [
 ]
 
 
+def normalize_vpn_id(vpnid):
+    """
+    Normalize a VPN identifier
+    Return a string based on the input string that can be used in a filename or
+    URL.
+    """
+    return vpnid.replace(" ","").replace("/","-")
+
+
 def parse_tops_vpn_csv_row(dictionary):
     """
     Parse a TOPS VPN CSV row
@@ -45,6 +54,7 @@ def parse_tops_vpn_csv_row(dictionary):
     the categories, where each category contains individual features. Note that
     if we find a feature that doesn't seem to match a category, it is added
     directly.
+    It also adds an 'id' field which can be used in a filename or URL parameter
     """
     outdict = {}
     for key, value in dictionary.items():
@@ -59,6 +69,7 @@ def parse_tops_vpn_csv_row(dictionary):
                 outdict[category][subkey] = value
         if not found_category:
             outdict[key] = value
+    outdict['id'] = normalize_vpn_id(outdict['VPN SERVICE'])
     return outdict
 
 
@@ -71,23 +82,39 @@ def extract_tops_vpn(filename):
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile, dialect="TopsVpn")
         for row in reader:
-            providers += [parse_tops_vpn_dictionary(row)]
+            providers += [parse_tops_vpn_csv_row(row)]
     return providers
 
 
 def save_vpns_as_json(vpnlist, outdir):
     """
     Save a list of dictionaries to a set of files on the filesystem
-    Use the VPN SERVICE key of the dictionary as the filename
+    Use the VPN SERVICE key of the dictionary as the filename.
+    Also saves a "vpns.json" file with a list of VPN IDs for the "core.vpns"
+    module to use.
     """
+
+    vpnsummaries = []
     for vpn in vpnlist:
-        filename = "{}.json".format(vpn['VPN SERVICE']).replace(' ', '').replace('/','-')
+        filename = "{}.json".format(vpn['id'])
         outpath = os.path.abspath(os.path.join(outdir, filename))
         with open(outpath, 'w') as outfile:
             json.dump(
                 vpn, outfile,
                 indent=2, skipkeys=False, sort_keys=True, ensure_ascii=True)
             print('', file=outfile)  # Add a newline
+
+        vpnsummaries += [{
+            'id': vpn['id'],
+            'VPN SERVICE': vpn['VPN SERVICE']
+        }]
+
+    summariespath = os.path.abspath(os.path.join(outdir, 'vpns.json'))
+    with open(summariespath, 'w') as summariesfile:
+        json.dump(
+            vpnsummaries, summariesfile,
+            indent=2, skipkeys=False, sort_keys=True, ensure_ascii=True)
+        print('', file=summariesfile)  # Add a newline
 
 
 def main(*args, **kwargs):
