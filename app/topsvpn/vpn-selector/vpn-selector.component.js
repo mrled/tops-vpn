3e689c25cd35099c62f857f60351d55b835a6cb1
Micah R Ledbetter <mrled@younix.us>
2016-07-13 12:58:01 -0500

    Use md-toolbar, md-chips, and md-list in vpn-selector

--- app/topsvpn/vpn-selector/vpn-selector.component.js
+++ app/topsvpn/vpn-selector/vpn-selector.component.js
@@ -7,16 +7,19 @@ angular.
     controller: ['$q', 'VpnData', 'MrlUtil',
       function ($q, VpnData, MrlUtil) {
         var self = this;
+
         self.vpns = [];
         self.featuresByCategory = {};
         self.featureValuesById = {};
+
         VpnData.getVpns().then(function(vpns) {
           self.vpns = vpns;
         });
+
         VpnData.mapCategoriesToFeatureIds().then(function(categoriesToFeatureIdsMap) {
           self.featuresByCategory = categoriesToFeatureIdsMap;
         });
-        var featureIdToValueMap;
+
         VpnData.mapFeatureIdsToValues().then(function(featureIdsToValuesMap) {
           self.featureValuesById = featureIdsToValuesMap;
         });
