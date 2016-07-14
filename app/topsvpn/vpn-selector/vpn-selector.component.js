21e151d0f6f31829d55cca20bf31d54236e0636a
Micah R Ledbetter <mrled@younix.us>
2016-07-14 14:07:00 -0500

    Redesign vpnSelector
    - Use proper Material elements
    - Two-columns
    - Prepare for actual selecting!

--- app/topsvpn/vpn-selector/vpn-selector.component.js
+++ app/topsvpn/vpn-selector/vpn-selector.component.js
@@ -4,25 +4,34 @@ angular.
   module('topsvpn').
   component('vpnSelector', {
     templateUrl: 'topsvpn/vpn-selector/vpn-selector.template.html',
-    controller: ['$q', 'VpnData', 'MrlUtil',
-      function ($q, VpnData, MrlUtil) {
+    controller: ['$scope', '$window', 'VpnData', 'MrlUtil',
+      function ($scope, $window, VpnData, MrlUtil) {
         var self = this;
 
         self.vpns = [];
         self.featuresByCategory = {};
         self.featureValuesById = {};
 
+        $scope.navigateToVpn = function(vpnId) {
+          var vpnRelativePath = '#!/vpns/' + vpnId;
+          var newLoc = $window.location.href.replace(/\#\!.*/, vpnRelativePath);
+          $window.location.href = newLoc;
+        };
+
         VpnData.getVpns().then(function(vpns) {
           self.vpns = vpns;
         });
 
         VpnData.mapCategoriesToFeatureIds().then(function(categoriesToFeatureIdsMap) {
+          // It is probably not useful to be able to filter on company names/URLs/etc
+          delete categoriesToFeatureIdsMap.company;
           self.featuresByCategory = categoriesToFeatureIdsMap;
         });
 
         VpnData.mapFeatureIdsToValues().then(function(featureIdsToValuesMap) {
           self.featureValuesById = featureIdsToValuesMap;
         });
+
       }
     ]
   });
