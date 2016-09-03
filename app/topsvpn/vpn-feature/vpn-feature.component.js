a458d73f0486546ab50be44e75be94a4c98337bb
Micah R Ledbetter <mrled@younix.us>
2016-09-03 16:24:12 -0500

    Finally actually *use* vpn-feature
    - Simplify vpn-selector
    - Make code more reusable
    - Make individual classes responsible for less

--- app/topsvpn/vpn-feature/vpn-feature.component.js
+++ app/topsvpn/vpn-feature/vpn-feature.component.js
@@ -3,14 +3,20 @@
 angular.module('topsvpn').
   component('vpnFeature', {
     templateUrl: 'topsvpn/vpn-feature/vpn-feature.template.html',
-    controller: ['VpnData', function(VpnData) {
-      var self = this;
-      VpnData.getFeatureValues(self.featureId).then(
+    controller: ['VpnData', '$scope', function(VpnData, $scope) {
+
+      // TODO: Not sure about this. Might should be a lookup from a global table / database?
+      function featureNameFromId(featureId) {
+        return featureId.replace(/.*\//, '');
+      }
+      $scope.$ctrl.featureName = featureNameFromId($scope.$ctrl.featureId);
+
+      VpnData.getFeatureValues($scope.$ctrl.featureId).then(
         function(featureValues) {
-          self.featureValues = featureValues;
+          $scope.$ctrl.featureValues = featureValues;
         },
         function(err) {
-          // console.log("Failed to get feature values for id '" + self.featureId + "' with error '" + err + "'.");
+          // console.log("Failed to get feature values for id '" + $scope.$ctrl.featureId + "' with error '" + err + "'.");
         }
     );
 
--- app/topsvpn/vpn-selector/vpn-selector.component.js
+++ app/topsvpn/vpn-selector/vpn-selector.component.js
@@ -8,55 +8,26 @@ angular.
       function ($q, $scope, $window, Vpn, VpnData, MrlUtil) {
         var self = this;
 
-        self.vpnsPromise = VpnData.getVpns();
-        self.featuresByCategoryPromise = VpnData.mapCategoriesToFeatureIds();
-        self.featureValuesByIdPromise = VpnData.mapFeatureIdsToValues();
-
         self.vpns = [];
         self.featuresByCategory = {};
-        self.featureValuesById = {};
-        self.features = [];
 
-        self.featureNameFromId = function(featureId) {
-          return featureId.replace(/.*\//, '');
-        };
-
+        // An on-click handler for a VPN button
         $scope.navigateToVpn = function(vpnId) {
           var vpnRelativePath = '#!/vpns/' + vpnId;
           var newLoc = $window.location.href.replace(/\#\!.*/, vpnRelativePath);
           $window.location.href = newLoc;
         };
 
-        self.vpnsPromise.then(function(vpns) {
+        VpnData.getVpns().then(function(vpns) {
           self.vpns = vpns;
         });
 
-        self.featuresByCategoryPromise.then(function(categoriesToFeatureIdsMap) {
+        VpnData.mapCategoriesToFeatureIds().then(function(categoriesToFeatureIdsMap) {
+          // TODO: clean up source data, so that I can do something more generic here
           // It is probably not useful to be able to filter on company names/URLs/etc
           delete categoriesToFeatureIdsMap.company;
           self.featuresByCategory = categoriesToFeatureIdsMap;
         });
-
-        $q.all([self.vpnsPromise, self.featureValuesByIdPromise]).then(function(resolved){
-          var rVpns = resolved[0];
-          var rFeatureValuesById = resolved[1];
-          for (var featureId in rFeatureValuesById) {
-            var name = Vpn.getFeatureIdNameCategory(featureId).name;
-            var category = Vpn.getFeatureIdNameCategory(featureId).category;
-            var type = rVpns[0].getFeature(category, name).type;
-            var valueList = rFeatureValuesById[featureId];
-
-            // Create a new VpnFeature object, but the value will be the list of all values
-            var newFeature = new Vpn.VpnFeature(name, category, valueList, type);
-
-            self.features.push(newFeature);
-          }
-        });
-
-        self.featureValuesByIdPromise.then(function(featureIdsToValuesMap) {
-          self.featureValuesById = featureIdsToValuesMap;
-        });
-
       }
     ]
   });
